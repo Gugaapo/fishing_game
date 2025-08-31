53e98a249f9af60e4afe4dc45ba14a72c40486fc
alt_merklein <galtmerklein@gmail.com>
2025-08-31 04:53:11 -0300

    fix export resource issue (thank god)

--- scripts/event_scripts/EventDB.gd
+++ scripts/event_scripts/EventDB.gd
@@ -1,68 +1,62 @@
-# EventDB.gd (autoload)
 extends Node
 
-const EVENTS_DIR := "res://events_data"  # <-- set to your actual folder
+var events: Dictionary = {}
+var _name_lut: Dictionary = {}
+var _loaded: bool = false
 
-var events: Dictionary = {}          # exact name -> EventData
-var _name_lut: Dictionary = {}       # lowercase -> exact name
-var _loaded := false
+@onready var _preloader: ResourcePreloader = get_node_or_null("Preloader") as ResourcePreloader
 
 func _ready() -> void:
 	_load_events()
-
-func _ensure_loaded() -> void:
-	if not _loaded:
-		_load_events()
 
 func _load_events() -> void:
 	events.clear()
 	_name_lut.clear()
 	_loaded = false
 
-	var dir := DirAccess.open(EVENTS_DIR)
-	if dir == null:
-		push_error("EventDB: can't open dir: %s" % EVENTS_DIR)
+	if _preloader == null:
+		push_error("EventDB: Preloader node not found. Make sure you autoload **EventDB.tscn** (a scene) whose root has this script and a child ResourcePreloader named 'Preloader'.")
+		_loaded = true
 		return
 
-	dir.list_dir_begin()
-	var fname := dir.get_next()
-	var count := 0
-	while fname != "":
-		if not dir.current_is_dir() and fname.ends_with(".tres"):
-			var path := EVENTS_DIR.path_join(fname)
-			var res := ResourceLoader.load(path, "EventData")  # type hint helps if class_name EventData exists
-			if res == null:
-				res = ResourceLoader.load(path)
-
-			if res is EventData:
-				var key := String(res.name).strip_edges()
-				if key.is_empty():
-					key = fname.get_basename()
-				events[key] = res
-				_name_lut[key.to_lower()] = key
-				count += 1
-			else:
-				var type_name := "null"
-				if res != null:
-					type_name = res.get_class()
-				push_warning("EventDB: skipping %s (type: %s)" % [fname, type_name])
-		fname = dir.get_next()
-	dir.list_dir_end()
+	var list: PackedStringArray = _preloader.get_resource_list()
+	var i: int = 0
+	var loaded_count: int = 0
+	while i < list.size():
+		var key_name: String = list[i]
+		var res: Resource = _preloader.get_resource(key_name)
+		if res is EventData:
+			_register_event(res)
+			loaded_count += 1
+		else:
+			var type_name: String = "null" if res == null else res.get_class()
+			push_warning("EventDB: preloader entry '%s' is not EventData (type=%s)" % [key_name, type_name])
+		i += 1
 
 	_loaded = true
-	print("EventDB: loaded %d events from %s" % [count, EVENTS_DIR])
-	if count > 0:
+	print("EventDB: loaded %d events from preloader" % loaded_count)
+	if loaded_count > 0:
 		print("EventDB keys:", events.keys())
 
-func get_event(event_name: String) -> EventData:
-	_ensure_loaded()
-	if events.has(event_name):
-		return events[event_name]
-	var lc := event_name.to_lower()
+func _register_event(ev: EventData) -> void:
+	var key: String = String(ev.name).strip_edges()
+	if key.is_empty():
+		var rp: String = ev.resource_path
+		key = rp.get_file().get_basename()
+	events[key] = ev
+	_name_lut[key.to_lower()] = key
+
+func get_event(name: String) -> EventData:
+	if not _loaded:
+		_load_events()
+	if events.has(name):
+		return events[name]
+	var lc: String = name.to_lower()
 	if _name_lut.has(lc):
 		return events[_name_lut[lc]]
 	return null
 
 func list_event_names() -> PackedStringArray:
-	_ensure_loaded()
+	if not _loaded:
+		_load_events()
 	return PackedStringArray(events.keys())
